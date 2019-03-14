358184f9f17bd35da840c2c0b5788377470aa3cc
SanchoPanzsa <zelgadis4@gmail.com>
2019-03-14 22:15:51 +0300

    Completed Quote Assembler. Several tests in future

--- QuoteAssembly/src/csv/MainClass.java
+++ QuoteAssembly/src/csv/MainClass.java
@@ -1,15 +1,31 @@
 package csv;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import quote.QuoteAssembler;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		String path = "C:\\Users\\LemonadeCandy\\OneDrive\\Wikia\\League of Legends voices\\Soraka\\Lines.csv";
+		String path = "C:\\Users\\Вадим\\OneDrive\\Wikia\\League of Legends voices\\Soraka\\Lines.csv";
 		QuoteAssembler qa = new QuoteAssembler();
-		qa.generateWikiText(path);
+		String result = qa.generateWikiText(path);
+		Path output = Paths.get("\\result.txt");
+		System.out.println(output.toAbsolutePath().toString());
+		
+		try(OutputStream os = Files.newOutputStream(output, StandardOpenOption.CREATE)) {
+			os.write(result.getBytes());
+			os.flush();
+			os.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- QuoteAssembly/src/quote/QuoteAssembler.java
+++ QuoteAssembly/src/quote/QuoteAssembler.java
@@ -1,9 +1,12 @@
 package quote;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import csv.CSVReader;
 
@@ -57,16 +60,42 @@ public class QuoteAssembler {
 		});
 		
 		// Начало игры
-		if (!startQuotes.isEmpty()) {
-			wikiText.append("\n== Начало матча ==\n");
-			startQuotes.stream().map(this::wrapIn).forEach(wikiText::append);
-		}
+		appendSection(wikiText, startQuotes, "Начало игры");
+		
 		// Атака
-		wikiText.append("\n== Атака ==\n");
-		attackQuotes.stream().map(this::wrapIn).forEach(wikiText::append);
-		System.out.println(wikiText.toString());
+		appendSection(wikiText, attackQuotes, "Атака");
+		
+		// Умения
+		appendSection(wikiText, abilityQuotes, "Использование умений");
+		
+		// Передвижение
+		appendSection(wikiText, movementQuotes, "Передвижение");
+		
+		// Шутки
+		appendSection(wikiText, jokes, "Шутки");
+		
+		// Насмешки
+		appendSection(wikiText, taunts, "Насмешки");
+		
+		// Убийства
+		appendSection(wikiText, kills, "Убийства");
+		
+		// При Возвращении
+		appendSection(wikiText, recall, "При {{si|Возвращение|Возвращении}}");
+		
+		// Другие фразы
+		appendSection(wikiText, otherQuotes, "Другие фразы");
+		
+		// Смех
+		appendSection(wikiText, laugh, "Смех");
+		
+		// Смерть
+		appendSection(wikiText, death, "Смерть");
+		
+		// Несортированное
+		appendSection(wikiText, unsortedQuotes, "Несортированное");
+		
 		return wikiText.toString();
-		//TODO: Завершить
 	}
 	
 	private void distributeQuotes() {		
@@ -112,10 +141,19 @@ public class QuoteAssembler {
 				return "Ошибка: число образов меньше числа файлов\n";
 			int last = soundFiles.length - 1;
 			for(int i = 0; i < last; i++) {
-				stroke += "{{фч|" + line.get(0) + soundFiles[i] + ".ogg}} ";
+				if(i > 0) {
+					stroke += "{{фч|" + line.get(0) + soundFiles[i] + ".ogg" +
+							"||" + line.get(0) + "|" + skins[i] + "}} ";
+				} else
+					stroke += "{{фч|" + line.get(0) + soundFiles[i] + ".ogg||"
+							+ line.get(0) + "|" + skins[i] + "}} ";
 			}
 			stroke += "{{фч|" + line.get(0) + soundFiles[last] + ".ogg|"
 					+ line.get(1);
+			if(last != 0) {
+				stroke += "|" + line.get(0) + "|" + skins[last];
+			}
+			
 			if(line.get(line.size() - 2).equals("Смех") ||
 					line.get(line.size() - 2).equals("Смерть") &&
 					line.get(1).startsWith("Предсмертн"))
@@ -123,5 +161,32 @@ public class QuoteAssembler {
 			stroke += "}}\n";
 		}
 		return stroke;
+	}
+	
+	private void appendSection(StringBuilder text, List<List<String>> section, String sectionName) {
+		if (!section.isEmpty()) {
+			text.append("\n== " + sectionName + " ==\n");
+			section.stream()
+				.filter(x -> x.get(x.size() - 1).equals("Нет"))
+				.map(this::wrapIn)
+				.forEach(text::append);
+			
+			Set<String> subSections = section.stream()
+				.map(x -> x.get(x.size() - 1))
+				.filter(x -> !x.equals("Нет"))
+				.distinct()
+				.collect(Collectors.toSet());
+			
+			if(subSections.size() > 0) {
+				subSections.forEach(x -> {
+					text.append(";")
+					.append(x + "\n");
+					section.stream()
+						.filter(y -> { return y.get(y.size() - 1).equals(x); })
+						.map(this::wrapIn)
+						.forEach(text::append);
+				});
+			}
+		}
 	}
 }
